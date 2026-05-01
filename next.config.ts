b622d7bb23ca3e61500ca973fc0efdd00a427f38
AashrayS <aashrayshukla189@gmail.com>
2026-05-01 14:52:24 +0530

    chore: update Next.js to v16 and React to v19

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Don't let ESLint errors fail the production build on Vercel
-    ignoreDuringBuilds: true,
-  },
+  // eslint: {
+  //   // Don't let ESLint errors fail the production build on Vercel
+  //   ignoreDuringBuilds: true,
+  // },
   typescript: {
     // Temporarily ignore TS errors to see if the build finishes on Vercel
     ignoreBuildErrors: true,
