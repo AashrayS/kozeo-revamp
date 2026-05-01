d85518a8d1d2f3d8ea5ed9b61a721607c74b9e10
AashrayS <aashrayshukla189@gmail.com>
2026-05-01 14:44:51 +0530

    feat: use env var for WEBSOCKET_URL to support dynamic deployment environments

--- src/config.js
+++ src/config.js
@@ -1,11 +1,9 @@
 // config.js
 
 // Use NEXT_PUBLIC_WEBSOCKET_URL for client-side config
-// export const WEBSOCKET_URL =
-//   process.env.NEXT_PUBLIC_WEBSOCKET_URL ||
-//   "wss://kozeo-ws-production.up.railway.app";
-//dev endpoint
-export const WEBSOCKET_URL =  "wss://kozeo-ws-production.up.railway.app";
+export const WEBSOCKET_URL =
+  process.env.NEXT_PUBLIC_WEBSOCKET_URL ||
+  "wss://kozeo-ws-production.up.railway.app";
 
 // GraphQL API endpoint
 export const GRAPHQL_URL =
