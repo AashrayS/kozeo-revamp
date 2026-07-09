8ec9318daa6daa393923c3110ec5843107e4d47f
AashrayS <aashrayshukla189@gmail.com>
2026-07-09 15:09:44 +0530

    chore: refactor ESLint configuration to use core-web-vitals directly and update lint script

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,8 @@
-import { FlatCompat } from "@eslint/eslintrc";
-
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-});
+import nextConfig from "eslint-config-next/core-web-vitals";
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
+  ...nextConfig,
 ];
 
 export default eslintConfig;
+
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // eslint: {
-  //   // Don't let ESLint errors fail the production build on Vercel
-  //   ignoreDuringBuilds: true,
-  // },
   typescript: {
     // Temporarily ignore TS errors to see if the build finishes on Vercel
     ignoreBuildErrors: true,
